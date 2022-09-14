3ed173a2fa36821b381c60d345c9a10b1f0bef86
Steve Fenton <99181436+steve-fenton-octopus@users.noreply.github.com>
2022-09-14 11:10:12 +0100

    Fix require reference errpr

--- lib/fenton-jekyll-plugin.rb
+++ lib/fenton-jekyll-plugin.rb
@@ -2,7 +2,7 @@ require "fenton-jekyll-plugin/jb_jekyll_breadcrumbs"
 require "fenton-jekyll-plugin/jb_jekyll_markdown"
 require "fenton-jekyll-plugin/jb_jekyll_paging"
 require "fenton-jekyll-plugin/jb_liquid_language"
-require "fenton-jekyll-plugin/regex"
+require "fenton-jekyll-plugin/jb_liquid_regex"
 
 module Jekyll
     module Fenton
